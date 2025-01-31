d97dbcd7e40c780d4183496f57c5e950590b1b1c
sasiik <sstupko31@gmail.com>
2025-01-31 17:50:07 +0200

    try again

--- print.py
+++ print.py
@@ -1,5 +1,5 @@
 print("I am somewhat familiar with Git")
-print("Now there should be a conflict")
+print("Now there shoulda")
 print("Making the conflict from idk branch")
 print("Changing the code in development branch for task 10")
 print("Changing the file again somehow")
