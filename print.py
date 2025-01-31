90f4f42369e8e9650a0956ad4efbf0342d997997
sasiik <sstupko31@gmail.com>
2025-01-31 16:15:43 +0200

    new lines added for task 20

--- print.py
+++ print.py
@@ -2,3 +2,6 @@ print("I am somewhat familiar with Git")
 print("Changing the file somehow")
 print("Changing the code in development branch for task 10")
 print("Changing the file again somehow")
+
+
+print("Making changes in the file for task 20")
