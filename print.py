bbaf1a8940c981472e4908038b25c310ac1af135
sasiik <sstupko31@gmail.com>
2025-01-31 17:25:45 +0200

    Making a conflict from idk branch

--- print.py
+++ print.py
@@ -1,5 +1,6 @@
 print("I am somewhat familiar with Git")
 print("Changing the file somehow")
+print("Making the conflict from idk branch")
 print("Changing the code in development branch for task 10")
 print("Changing the file again somehow")
 
