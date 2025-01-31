1ff0ac5d017f48d635b91067f44c538ce8d44fa5
sasiik <sstupko31@gmail.com>
2025-01-31 17:29:04 +0200

    Now for sure conflict from idk

--- print.py
+++ print.py
@@ -1,5 +1,5 @@
 print("I am somewhat familiar with Git")
-print("Now there should be a conflict")
+print("Now there shouldadlk;fja;sdfj;adjfkl be a conflict")
 print("Making the conflict from idk branch")
 print("Changing the code in development branch for task 10")
 print("Changing the file again somehow")
